b523b8b05c25b53d43bc16335f21f271c32f1c2b
grahamderry <51369925+GrahamDerry@users.noreply.github.com>
2025-06-15 13:15:23 +0000

    commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./styles.css";
+import ThaiVocabTable from "./components/ThaiVocabTable";
 
 export default function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
+
+      {/* keep App tidy—table lives in its own component */}
+      <ThaiVocabTable />
     </div>
   );
 }
